d0c61c60294863a3d644094b07f67ce07110bf38
brcthn <burcu1818@gmail.com>
2021-02-02 11:52:23 +0100

    Created basic tetrominoes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import"./reset.css";
 import "./style.css";
+import {createGrid} from "./functions/grid";
+import {tetrominoes} from "./constants/tetrominoes"
+import { GRID_WIDTH } from "./constants/grid";
+const blocks = createGrid();
+
+const getRandomNumber=(max)=>{
+   const randomNum= Math.floor(Math.random()*max);
+    return randomNum;
+}
+const currentTetromino=tetrominoes[getRandomNumber(5)];
+const currentRotation=currentTetromino[getRandomNumber(currentTetromino.length)];
+const startingPoint=Math.floor((GRID_WIDTH/2)-2);
+
+currentRotation.forEach(index => {
+    blocks[ startingPoint+index].classList.add("filled");
+    
+});
+
